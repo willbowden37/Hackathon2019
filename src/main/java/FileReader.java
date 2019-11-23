5b1271e0fe0b1c1eff8849b2e87d60a12b502426
willbowden37 <william.bowden@aggiemail.usu.edu>
2019-11-22 17:07:41 -0700

    Final progress

--- src/main/java/FileReader.java
+++ src/main/java/FileReader.java
@@ -1,8 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public abstract class FileReader {
     public final String read() {
-        return readType() + "," + readService() + "," + readPassword();
+        String sizeString = readSize();
+        try {
+            int size = Integer.parseInt(sizeString);
+            ArrayList<String> passwords = new ArrayList<>();
+            for(int i = 0; i < size; size++) {
+                passwords.add(readType() + "," + readService() + "," + readPassword());
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("File Invalid");
+            return null;
+        }
+        return null;
     }
 
+    protected Scanner inputScanner;
+
+    protected abstract String readSize();
     protected abstract String readType();
     protected abstract String readService();
     protected abstract String readPassword();
--- src/main/java/TxtReader.java
+++ src/main/java/TxtReader.java
@@ -1,2 +1,33 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class TxtReader extends FileReader {
+    public TxtReader(String fileName) {
+        try {
+            File file = new File(fileName);
+            inputScanner = new Scanner(file);
+        } catch (Exception ignore) {
+
+        }
+    }
+
+    @Override
+    protected String readSize() {
+        return inputScanner.nextLine();
+    }
+
+    @Override
+    protected String readType() {
+        return inputScanner.nextLine();
+    }
+
+    @Override
+    protected String readService() {
+        return inputScanner.nextLine();
+    }
+
+    @Override
+    protected String readPassword() {
+        return inputScanner.nextLine();
+    }
 }
